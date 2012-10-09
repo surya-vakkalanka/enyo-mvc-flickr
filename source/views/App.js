508a8350bd40a5ce6d537cf867783d1ad381dc6c
Cole Davis <cole.davis@palm.com>
2012-10-09 14:09:47 -0700

    update basic hello world app

--- source/views/App.js
+++ source/views/App.js
@@ -1,10 +1,7 @@
-enyo.kind({
-	name: "App",
-	fit: true,
-	components:[
-		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.hello.addContent("<br/><b>hello</b> control was tapped");
-	}
+enyo.app({
+  name: "App",
+  controller: "App.AppController",
+  components: [
+    {name: "hello", content: "Hello World", allowHtml: true}
+  ]
 });
